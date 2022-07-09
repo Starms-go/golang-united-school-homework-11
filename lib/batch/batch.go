58c36ea372ec1e9cb323d6c8fccbc83390b2404b
Starms-go <evra3487@gmainl.com>
2022-07-09 22:02:49 +0300

    first attempt

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -14,5 +15,24 @@ func getOne(id int64) user {
 }
 
 func getBatch(n int64, pool int64) (res []user) {
-	return nil
+
+	var wg sync.WaitGroup
+	// var mx sync.Mutex
+
+	sem := make(chan struct{}, pool)
+	var i int64
+	for ; i < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+		go func(j int64) {
+			// mx.Lock()
+			curUser := getOne(j)
+			res = append(res, curUser)
+			// mx.Unlock()
+			<-sem
+			wg.Done()
+		}(i)
+	}
+	wg.Wait()
+	return
 }
